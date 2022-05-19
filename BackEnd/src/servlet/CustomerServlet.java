54ea749e9f74014e944450bb9d885317304adb23
MilashaThathsarani <milashathathsarni99@gmail.com>
2022-05-19 18:23:02 +0530

    Updated

---
+++ BackEnd/src/servlet/CustomerServlet.java
@@ -0,0 +1,178 @@
+package servlet;
+
+import javax.annotation.Resource;
+import javax.json.Json;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObjectBuilder;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@WebServlet (urlPatterns = "/customer")
+public class CustomerServlet extends HttpServlet {
+    @Resource(name = "java:comp/env/jdbc/pool")
+    DataSource dataSources;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        JsonArrayBuilder arrayBuilder = Json.createArrayBuilder();
+        JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+        JsonObjectBuilder dataMsgBuilder = Json.createObjectBuilder();
+        PrintWriter writer = resp.getWriter();
+
+        Connection connection = null;
+
+        try {
+            connection = dataSources.getConnection();
+            String option = req.getParameter("option");
+            switch (option) {
+                case "GetAll":
+                    ResultSet resultSet = connection.prepareStatement("select * from customer").executeQuery();
+                    while (resultSet.next()) {
+                        String id = resultSet.getString(1);
+                        String name = resultSet.getString(2);
+                        String address = resultSet.getString(3);
+                        int salary = resultSet.getInt(4);
+
+                        resp.setStatus(HttpServletResponse.SC_OK);//201
+
+                        objectBuilder.add("id", id);
+                        objectBuilder.add("name", name);
+                        objectBuilder.add("address", address);
+                        objectBuilder.add("salary", salary);
+
+                        arrayBuilder.add(objectBuilder.build());
+                    }
+
+                    dataMsgBuilder.add("data", arrayBuilder.build());
+                    dataMsgBuilder.add("massage", "Done");
+                    dataMsgBuilder.add("status", "200");
+
+                    writer.print(dataMsgBuilder.build());
+                    break;
+
+                case "GenId":
+                    ResultSet genRst = connection.prepareStatement("SELECT id FROM customer ORDER BY id DESC LIMIT 1").executeQuery();
+                    if (genRst.next()) {
+                        int tempId = Integer.parseInt(genRst.getString(1).split("-")[1]);
+                        tempId += 1;
+                        if (tempId < 10) {
+                            objectBuilder.add("id", "C00-00" + tempId);
+                        } else if (tempId < 100) {
+                            objectBuilder.add("id", "C00-0" + tempId);
+                        } else if (tempId < 1000) {
+                            objectBuilder.add("id", "C00-" + tempId);
+                        }
+                    } else {
+                        objectBuilder.add("id", "C00-000");
+                    }
+                    dataMsgBuilder.add("data", objectBuilder.build());
+                    dataMsgBuilder.add("message", "Done");
+                    dataMsgBuilder.add("status", 200);
+                    writer.print(dataMsgBuilder.build());
+                    break;
+
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        String id = req.getParameter("cusId");
+        String name = req.getParameter("custName");
+        String address = req.getParameter("cusAddress");
+        String salary = req.getParameter("cusSalary");
+
+        PrintWriter writer = resp.getWriter();
+        Connection connection = null;
+        try {
+            connection = dataSources.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO customer VALUES(?,?,?,?)");
+            preparedStatement.setObject(1, id);
+            preparedStatement.setObject(2, name);
+            preparedStatement.setObject(3, address);
+            preparedStatement.setObject(4, salary);
+
+            if (preparedStatement.executeUpdate() > 0) {
+                JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+                resp.setStatus(HttpServletResponse.SC_CREATED);
+                objectBuilder.add("status", 200);
+                objectBuilder.add("message", "customer Added success");
+                objectBuilder.add("data", "");
+                writer.print(objectBuilder.build());
+            }
+
+        } catch (SQLException throwables) {
+            JsonObjectBuilder objectBuilder = Json.createObjectBuilder();
+            resp.setStatus(HttpServletResponse.SC_OK);
+            objectBuilder.add("status", 400);
+            objectBuilder.add("message", "Error");
+            objectBuilder.add("data", throwables.getLocalizedMessage());
+            writer.print(objectBuilder.build());
+            throwables.printStackTrace();
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        String customerID = req.getParameter("customerID");
+        System.out.println("cus : " + " " + customerID);
+        JsonObjectBuilder dataMsgBuilder = Json.createObjectBuilder();
+        PrintWriter writer = resp.getWriter();
+
+        Connection connection = null;
+        try {
+            connection = dataSources.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("DELETE FROM customer WHERE id=?");
+            pstm.setObject(1, customerID);
+
+            if (pstm.executeUpdate() > 0) {
+                resp.setStatus(HttpServletResponse.SC_OK); //200
+                dataMsgBuilder.add("data", "");
+                dataMsgBuilder.add("massage", "Customer Deleted");
+                dataMsgBuilder.add("status", "200");
+                writer.print(dataMsgBuilder.build());
+            }
+        } catch (SQLException e) {
+            dataMsgBuilder.add("status", 400);
+            dataMsgBuilder.add("message", "Error");
+            dataMsgBuilder.add("data", e.getLocalizedMessage());
+            writer.print(dataMsgBuilder.build());
+            resp.setStatus(HttpServletResponse.SC_OK); //200
+        } finally {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+}
